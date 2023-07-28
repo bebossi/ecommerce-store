0ff3b929e97b3f0113fbe12fc83d2bfd7251c743
bebossi <bernardobossi77@gmail.com>
2023-07-28 11:37:20 -0300

    creating cart page with all funcionalities

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,6 +8,7 @@ import Currency from "./currency";
 import { useRouter } from "next/navigation";
 import { MouseEventHandler } from "react";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/UseCart";
 
 interface ProductCardProps {
   data: Product;
@@ -15,6 +16,7 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
   const previewModal = usePreviewModal()
+  const cart = useCart()
   const router = useRouter()
   const handleClick = () => {
     router.push(`/product/${data.id}`)
@@ -25,6 +27,14 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
 
     previewModal.onOpen(data)
   }
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation()
+
+    cart.addItem(data)
+  }
+
+
 
   return (
     <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
@@ -38,7 +48,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton onClick={onPreview} icon={<Expand size={20} className="text-gray-600"/>} />
-            <IconButton onClick={() => {}} icon={<ShoppingCart size={20} className="text-gray-600"/>} />
+            <IconButton onClick={onAddToCart} icon={<ShoppingCart size={20} className="text-gray-600"/>} />
 
           </div>
         </div>
---
+++ hooks/UseCart.ts
@@ -0,0 +1,41 @@
+import { Product } from "@/type";
+import { toast } from "react-hot-toast";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+interface CartStore {
+  items: Product[];
+  addItem: (data: Product) => void;
+  removeItem: (id: string) => void;
+  removeAll: () => void;
+}
+
+const useCart = create(
+  persist<CartStore>(
+    (set, get) => ({
+      items: [],
+      addItem: (data: Product) => {
+        const currentItems = get().items;
+        const existingItem = currentItems.find((item) => item.id === data.id);
+
+        if (existingItem) {
+          return toast("Item already in cart");
+        }
+
+        set({ items: [...get().items, data] });
+        toast.success("items added to cart");
+      },
+      removeItem: (id: string) => {
+        set({ items: [...get().items.filter((item) => item.id !== id)] });
+        toast.success("items removed from cart");
+      },
+      removeAll: () => set({ items: [] }),
+    }),
+    {
+      name: "cart-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
+
+export default useCart;
