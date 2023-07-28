ec56510fd4526732e607cf17270e2f412dd205b0
bebossi <bernardobossi77@gmail.com>
2023-07-28 07:16:37 -0300

    updating infos in product page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,6 @@
 import getProducts from "@/actions/get-products";
 import getProduct from "@/actions/getProduct";
+import Info from "@/components/Info";
 import Gallery from "@/components/gallery";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
@@ -18,14 +19,15 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
   const suggestedProducts = await getProducts({
     categoryId: product?.category?.id,
   });
-  console.log(product)
   return (
     <div className="bg-white ">
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             <Gallery images={product.images} /> 
-            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">Info</div>
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info data={product}/>
+              </div>
           </div>
           <hr className="my-10" />
             <ProductList title="Related items" items={suggestedProducts}/>
